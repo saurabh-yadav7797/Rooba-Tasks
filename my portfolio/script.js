b2997e21f1e1d31462dff17a3170596f01c0e097
saurabh-yadav7797 <saurabh1702042@gmail.com>
2024-05-07 11:13:32 +0530

    my portfolio by using html css javaScript and React

--- my portfolio/script.js
+++ my portfolio/script.js
@@ -1,8 +1,8 @@
 // Simulated data for projects
 const projects = [
-    { name: "Project 1", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", color: "#28a745" },
-    { name: "Project 2", description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", color: "#007bff" },
-    { name: "Project 3", description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", color: "#dc3545" }
+    { name: "Project 1", description: "blood bank using python django html css javaScipt", color: "#28a745" },
+    { name: "Project 2", description: "This project deals with developing a Virtual website ‘E-commerce Website’. It provides the user with a list of the various products available for purchase in the store. For the convenience of online shopping, a shopping cart is provided to the user. After the selection of the goods, it is sent for the order confirmation process. The system is implemented using Python’s web framework Django. To build a Django e-commerce web application, it is necessary to study and understand many technologies.", color: "#007bff" },
+    { name: "Project 3", description: "A College Website developed using HTML, CSS, and JavaScript is an interactive platform that provides information about the school, its faculty, courses, events, and other relevant details. It incorporates responsive design principles to ensure compatibility across various devices, offering an engaging user experience for students, parents, and staff.", color: "#dc3545" }
 ];
 
 // Function to generate project cards
